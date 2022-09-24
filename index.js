4664ff3991ffe964450e9f85c218487a269f3195
vardhan0604 <harsh062004@gmail.com>
2022-09-24 18:18:35 +0530

    project update

--- index.js
+++ index.js
@@ -2,16 +2,49 @@ let myLeads=[]
 const inputBtn = document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el");
 const ulEl=document.getElementById("ul-el");
+const dltBtn=document.getElementById("dlt-btn");
+const tabBtn=document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    output(myLeads);
+}
+
 
 inputBtn.addEventListener("click", function() {
+// pushes input value into tha array and clears the input box
     myLeads.push(inputEl.value);
-    console.log(myLeads)
     inputEl.value='';
-    
+
+    // store myleads in the local storage
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    console.log(localStorage.getItem("myLeads"));
+
+// call output function
+    output(myLeads)
 })
 
+
+function output(leads){
+    let listItems=""
     let i=0;
-    for(i=0;i<myLeads.length;i++){
-        ulEl.innerHTML += "<li>"+myLeads[i]+"</li>"
-        console.log(myLeads[i]);
+    for(i=0;i<leads.length;i++){
+        listItems +=` 
+        <li>
+            <a href=${leads[i]} target="_blank">
+                ${leads[i]}
+            </a>
+        </li>`
+        
     }
+    ulEl.innerHTML=listItems;
+
+}
+
+dltBtn.addEventListener("dblclick",function () {
+    localStorage.clear;
+    myLeads=[]
+     output(myLeads);
+
+})
